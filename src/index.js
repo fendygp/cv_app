135fb156e64a2bb4d58a9926c05fc6f5a0f495a4
fendypradana <fendy.pradana@work.bri.co.id>
2020-11-12 21:24:20 +0700

    bootstarp

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import "react-app-polyfill/ie11";
 import "react-app-polyfill/stable";
 import React from "react";
 import ReactDOM from "react-dom";
-import "bootstrap/dist/css/bootstrap-reboot.min.css";
-import "bootstrap/dist/css/bootstrap-grid.min.css";
+import "./bootstrap/dist/css/bootstrap-reboot.min.css";
+import "./bootstrap/dist/css/bootstrap-grid.min.css";
 import App from "./App";
 
 import * as serviceWorker from "./serviceWorker";
